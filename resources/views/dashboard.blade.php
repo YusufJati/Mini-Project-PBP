51403d5c97c3e0a4276256c238a127b4778cbb72
ririnindah <ririnkurin07@gmail.com>
2023-10-21 22:42:54 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,42 @@
 @extends('layouts.mainlayout')
 
 @section('title', 'Dashboard')
+
+@section('content')
+    <h5 class="fw-normal"><a href="/" class="text-decoration-none text-dark">Dashboard</a></h5>
+    <div class="d-flex">
+        <p class="ms-auto">
+            <a href="/" class="text-decoration-none">Dashboard</a>
+            <a href="/" class="text-decoration-none text-muted"> / Daftar Buku</a>
+        </p>
+    </div>
+
+    {{-- search --}}
+
+    
+
+    {{-- isi content --}}
+    <div class="container">
+        <div class="row">
+            {{-- foreach --}}
+            <div class="card mb-3 p-0" style="max-width: 300px;">
+                <div class="row g-0">
+                    <a href="#" class="text-decoration-none text-dark d-flex">
+                        {{-- gambar --}}
+                        <div class="images col-md-4">
+                            <img src="assets/dilan.jpg" class="img-fluid rounded-start" alt="...">
+                        </div>
+
+                        <div class="detail col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Judul Buku</h5>
+                                <p class="card-text">Pengarang</p>
+                                <p class="card-text">Penerbit</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -19,6 +19,10 @@
     {{-- navbar --}}
     @include('partials.navbar')
 
+    <div class="container mt-3">
+        @yield('content')
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
