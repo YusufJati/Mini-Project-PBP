10fe878f59965a1ac5f3480279b4f42783ae64ed
ririnindah <ririnkurin07@gmail.com>
2023-10-21 20:15:21 +0700

    navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,32 @@
+<div class="main">
+    <nav class="navbar navbar-expand-lg bg-info">
+        <div class="container-fluid mx-4">
+            <a class="navbar-brand" href="/">
+                <img src="assets/{{ $image }}" width="25">
+                Perpustakaan</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link {{ ($active === "dashboard") ?  'active': '' }}" href="/">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/koleksi">Koleksi</a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i>
+                            Login
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
+
