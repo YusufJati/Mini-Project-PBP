d11785d47efba437c56953ba42d0da2e359bec50
Yusuf <yusufjana18@gmail.com>
2023-10-27 14:59:44 +0700

    little update

--- resources/views/dashboard/anggota/login.blade.php
+++ resources/views/dashboard/anggota/login.blade.php
@@ -41,7 +41,7 @@
                             <button type="submit" class="btn btn-primary">Login</button>
                         </div>
                         <br>
-                        <a href="{{ route('anggota.register') }}"> Create new Account</a> 
+                        <a href="{{ route('anggota.register') }}"> Buat akun baru</a> 
                 </form>
             </div>
         </div>
