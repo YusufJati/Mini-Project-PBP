2d000397523cc51a7a0a34fa0179d665b25adcd8
hanashabrina <hanashabrinaa@gmail.com>
2023-10-27 11:00:45 +0700

    gabung tabel

--- resources/views/dashboard/anggota/riwayat.blade.php
+++ resources/views/dashboard/anggota/riwayat.blade.php
@@ -48,61 +48,51 @@
                         </table>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="card mt-5">
-        <div class="card-body">
-            <div class="tab-pane fade" id="berlangsung" role="tabpanel" aria-labelledby="berlangsung-tab">
-                <!--peminjaman yang sedang berlangsung-->
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead class="bg-primary text-white">
-                            <tr>
-                                <th scope="col">Judul Buku</th>
-                                <th scope="col">Tanggal Peminjaman</th>
-                                <th scope="col">Tanggal Jatuh Tempo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Bintang</td>
-                                <td>10-10-2023</td>
-                                <td>17-10-2023</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="tab-pane fade" id="berlangsung" role="tabpanel" aria-labelledby="berlangsung-tab">
+                    <!--peminjaman yang sedang berlangsung-->
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead class="bg-primary text-white">
+                                <tr>
+                                    <th scope="col">Judul Buku</th>
+                                    <th scope="col">Tanggal Peminjaman</th>
+                                    <th scope="col">Tanggal Jatuh Tempo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Bintang</td>
+                                    <td>10-10-2023</td>
+                                    <td>17-10-2023</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="card mt-5">
-        <div class="card-body">
-            <div class="tab-pane fade" id="terlambat" role="tabpanel" aria-labelledby="terlambat-tab">
-                <!--peminjaman yang terlambat-->
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead class="bg-primary text-white">
-                            <tr>
-                                <th scope="col">Judul Buku</th>
-                                <th scope="col">Tanggal Peminjaman</th>
-                                <th scope="col">Tanggal Jatuh Tempo</th>
-                                <th scope="col">Durasi Terlambat</th>
-                                <th scope="col">Denda</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Buku 4</td>
-                                <td>05-09-2023</td>
-                                <td>12-09-2023</td>
-                                <td>3 hari</td>
-                                <td>15000</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="tab-pane fade" id="terlambat" role="tabpanel" aria-labelledby="terlambat-tab">
+                    <!--peminjaman yang terlambat-->
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead class="bg-primary text-white">
+                                <tr>
+                                    <th scope="col">Judul Buku</th>
+                                    <th scope="col">Tanggal Peminjaman</th>
+                                    <th scope="col">Tanggal Jatuh Tempo</th>
+                                    <th scope="col">Durasi Terlambat</th>
+                                    <th scope="col">Denda</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Buku 4</td>
+                                    <td>05-09-2023</td>
+                                    <td>12-09-2023</td>
+                                    <td>3 hari</td>
+                                    <td>15000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
